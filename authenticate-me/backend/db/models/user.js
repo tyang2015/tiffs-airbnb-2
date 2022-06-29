6b3671ca83f222a11551a125995950d2b0d4ea06
tiffany yang <tiffanyang2015@gmail.com>
2022-06-29 14:08:30 -0700

    finished phase-3

---
+++ authenticate-me/backend/db/models/user.js
@@ -0,0 +1,111 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    toSafeObject(){
+      // we are only extract the 3 necessary fields when they create and send us the instance
+      const {id, username, email} = this;
+      return {id, username, email};
+    }
+
+    validatePassword(password) {
+      return bcrypt.compareSync(password, this.hashedPassword.toString());
+    }
+
+    static getCurrentUserById(id){
+      return User.scope("currentUser").findByPk(id)
+    }
+
+    static async login({ credential, password }) {
+      const { Op } = require('sequelize');
+      const user = await User.scope('loginUser').findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+      if (user && user.validatePassword(password)) {
+        return await User.scope('currentUser').findByPk(user.id);
+      }
+    }
+
+    static async signup({ username, email, password }) {
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({
+        username,
+        email,
+        hashedPassword
+      });
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+
+
+
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    username: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [4,30],
+        isEmail:false
+      }
+    },
+    email: {
+      type:DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 256],
+        isEmail: true
+      },
+    },
+    hashedPassword: {
+      type:DataTypes.STRING.BINARY,
+      allowNull: false,
+      validate:{
+        len: [60,60]
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ['hashedPassword','email', 'createdAt', "updatedAt"]
+      }
+    },
+    scopes:{
+      currentUser(){
+        return {
+          attributes: {
+            exclude: ['hashedPassword']
+          }
+        }
+      },
+      loginUser(){
+        return {
+          // can we clarify this part..
+          attributes: {}
+        }
+      }
+    }
+
+  });
+  return User;
+};
--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,4 +1,39 @@
 const router = require('express').Router()
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+
+// for testing user auth middleware
+// GET /api/set-token-cookie
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
+
+// ...
+
+router.get(
+    '/restore-user',
+    restoreUser,
+    (req, res) => {
+      return res.json(req.user);
+    }
+  );
+
+  router.get(
+    '/require-auth',
+    requireAuth,
+    (req, res) => {
+      return res.json(req.user);
+    }
+  );
 
 
 router.post('/test', (req,res)=>{
