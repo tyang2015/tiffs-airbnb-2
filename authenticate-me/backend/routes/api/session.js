e70ed1af6e642b6f1323df66396f0668bc3c690d
tiffany yang <tiffanyang2015@gmail.com>
2022-06-29 19:59:39 -0700

    finished validation feature

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -2,9 +2,25 @@
 const express = require('express')
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
+// phase 5
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { User } = require('../../db/models');
 
 const router = express.Router();
+
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
 
 router.get(
     '/',
@@ -21,6 +37,7 @@ router.get(
 
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
       const { credential, password } = req.body;
 
