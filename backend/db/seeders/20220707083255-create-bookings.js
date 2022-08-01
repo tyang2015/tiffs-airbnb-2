98b581e54c0bec0eb1397c0c746a309b582fd81b
tiffany yang <tiffanyang2015@gmail.com>
2022-07-31 17:57:36 -0700

    seeded again bookings data

--- backend/db/seeders/20220707083255-create-bookings.js
+++ backend/db/seeders/20220707083255-create-bookings.js
@@ -63,62 +63,62 @@ module.exports = {
     {
       spotId: 9,
       userId: 1,
-      startDate: "2022-05-11",
-      endDate: "2020-06-16"
+      startDate: new Date("2022-05-11"),
+      endDate: new Date("2020-06-16")
     },
     {
       spotId: 10,
       userId: 1,
-      startDate: "2024-09-10",
-      endDate: "2024-10-15"
+      startDate: new Date("2024-09-10"),
+      endDate: new Date("2024-10-15")
     },
     {
       spotId: 11,
       userId: 1,
-      startDate: "2023-01-01",
-      endDate: "2023-02-01"
+      startDate: new Date("2023-01-01"),
+      endDate: new Date("2023-02-01")
     },
     {
       spotId: 12,
       userId: 1,
-      startDate: "2029-08-10",
-      endDate: "2029-10-15"
+      startDate: new Date("2029-08-10"),
+      endDate: new Date("2029-10-15")
     },
     {
       spotId: 13,
       userId: 1,
-      startDate: "2028-08-10",
-      endDate: "2028-12-15"
+      startDate: new Date("2028-08-10"),
+      endDate: new Date("2028-12-15")
     },
     {
       spotId: 2,
       userId: 2,
-      startDate: "2026-07-06",
-      endDate: "2026-07-09"
+      startDate: new Date("2026-07-06"),
+      endDate: new Date("2026-07-09")
     },
     {
       spotId: 2,
       userId: 1,
-      startDate: "2031-10-10",
-      endDate: "2031-12-17"
+      startDate: new Date("2031-10-10"),
+      endDate: new Date("2031-12-17")
     },
     {
       spotId: 1,
       userId: 2,
-      startDate: "2029-11-10",
-      endDate: "2029-11-15"
+      startDate: new Date("2029-11-10"),
+      endDate: new Date("2029-11-15")
     },
     {
       spotId: 10,
       userId: 4,
-      startDate: "2028-11-10",
-      endDate: "2028-11-15"
+      startDate: new Date("2028-11-10"),
+      endDate: new Date("2028-11-15")
     },
     {
       spotId: 12,
       userId: 4,
-      startDate: "2025-12-10",
-      endDate: "2025-12-15"
+      startDate: new Date("2025-12-10"),
+      endDate: new Date("2025-12-15")
     }
   ])
   },
