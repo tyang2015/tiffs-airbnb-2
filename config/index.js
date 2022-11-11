96a7bec042c44f631c85f833fce79a4ba95305fb
tiffany yang <tiffanyang2015@gmail.com>
2022-11-10 19:57:49 -0800

    integrated map and styled marker

--- config/index.js
+++ config/index.js
@@ -8,5 +8,6 @@ module.exports = {
         secret: process.env.JWT_SECRET,
         expiresIn: process.env.JWT_EXPIRES_IN
     },
+    googleMapsAPIKey: process.env.MAPS_API_KEY
 
 }
--- routes/api/spots.js
+++ routes/api/spots.js
@@ -390,7 +390,7 @@ router.post('/:spotId/bookings', requireAuth ,validateBooking, async(req,res,nex
 // get bookings booked by user for a given spot
 router.get('/:spotId/bookings', async(req, res, next)=>{
     let bookings = await Booking.findAll({
-      include: [{model: Spot, attributes: ['previewImage', 'ownerId']}, {model:User, attributes:
+      include: [{model: Spot}, {model:User, attributes:
          {exclude: ['email', 'hashedPassword', 'createdAt', 'updatedAt']}}],
       where: {spotId: req.params.spotId}
     });
